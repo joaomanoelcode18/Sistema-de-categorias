a18bcd949c07f78e25480317119defc75b60e65a
joao <joaomanoel2006f@gmail.com>
2025-08-10 14:43:11 -0300

    cadastro mudado

--- helpers/eAdmin.js
+++ helpers/eAdmin.js
@@ -1,6 +1,6 @@
 module.exports ={
     eAdmin: function (req, res, next){
-        if(req.isAuthenticated() && req.user.eAdmin == 1){
+        if(req.isAuthenticated()){
            return next();
         }
         req.flash("error_msg", "você precisa ser um admin")
